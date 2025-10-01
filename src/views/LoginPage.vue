89feea04dd25d44aa9dfd54baab1290b4c99a3da
Nana Lee <nanalee@pal-nat186-156-40.itap.purdue.edu>
2025-10-01 16:44:48 -0400

    #1 Refactor InputField

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,9 +1,10 @@
 <template>
 
     <div> 
-        <input type="test" :placeholer="Email" v-model="email">
-        <input type="password" :placeholer="Password" v-mode="password"/>
-
+        <!--input type="test" :placeholer="email" v-model="email">
+        <input type="password" :placeholer="password" v-mode="password"/-->
+        <InputField :type="'text'" :placeholder="'Email'" :modelValue="email" v-model="email"/>
+        <InputField :type="'password'" :placeholder="'Password'" :modelValue="password" />
         <button @click="login">로그인하기</button>
     </div>
     <div>
@@ -14,8 +15,11 @@
 </template>
 
 <script>
+import InputField from '@/components/InputField.vue';
+
 export default {
     name: "LoginPage",
+    components: {InputField},
     data() {
         return {
             email: "",
